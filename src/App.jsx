dcc725f4729d26f15110a4ce942592bfb80f4ba4
shuxratov2024 <shukhratovamir010@gmail.com>
2025-01-12 15:06:50 +0500

    uzum-1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Banner from "./Components/Banner"
+import Header from "./Components/Header"
+
+function App() {
+
+  return (
+    <>
+    <div className="container">
+    <Header/>
+    <Banner/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../styles/_banner.scss"
+
+function Banner() {
+  return (
+    <>
+    <div className="banner">
+
+
+    </div>
+    </>
+  )
+}
+
+export default Banner
